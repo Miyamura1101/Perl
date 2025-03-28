69ac134615b12af9bf3370f95f1ef487c45f24bd
Miyamura <felipesilsou1101@gmail.com>
2025-03-28 17:54:06 -0300

    Mudanças

--- Projeto/Procura_Chaves.pl
+++ Projeto/Procura_Chaves.pl
@@ -3,9 +3,12 @@ use warnings;
 use utf8;   # Suporte a caracteres acentuados
 use open qw(:std :utf8);  # Garante que a leitura e escrita estejam em UTF-8
 
-my %palavrasReservadas = map { $_ => 1 } qw(a à às ao aos o os e de desde da das do dos em entre dentro com consigo 
-contigo conosco para perante por perante através um uma uns umas que quem cujo cuja cujos cujas se seu sua seus suas deles delas
-é são está estão era eram na nas no nos os as não ou sim certamente ao aos mais maior maioria mas porém contudo todavia cada qualquer);
+my %palavrasReservadas = map { $_ => 1 } qw(a à às ao aos o os e de desde da das do dos em entre dentro com consigo como ele eles ela elas
+contigo conosco para perante por perante através um uma uns umas que quem cujo cuja cujos cujas se seu sua seus suas deles delas pela pelas
+é são está estão era eram na nas no nos os as não ou sim certamente ao aos mais maior maioria mas porém contudo todavia cada qualquer que faz
+isso isto aquele aquela aqueles aquelas há havia sob sobre durante entre após antes desde contra perante até bem mal pouco poucos com por qual 
+pouca poucas muito muitos muita muitas então depois ainda quando enquanto porque seja sendo fui foi fomos foram me te se nos vos lhe lhes quais
+mãe todo toda todos todas);
 
 my %frequencia;
 
@@ -24,6 +27,7 @@ while (my $linha = <$arq>) {
 
     foreach my $palavra (@palavras) {
         next if exists $palavrasReservadas{$palavra};  # Pega a lista de pelavras e retira da frequencia
+        next if $palavra =~ /^\d+$/;     # Para ignorar numeros
         $frequencia{$palavra}++;
     }
 }
--- Projeto/Procura_Frequencia.pl
+++ Projeto/Procura_Frequencia.pl
@@ -2,11 +2,12 @@ use strict;
 use utf8;
 use open qw(:std :utf8);
 
-my %palavrasReservadas = map { $_ => 1 } qw(a à às ao aos o os e de desde da das do dos em entre dentro com consigo como
-contigo conosco para perante por perante através um uma uns umas que quem cujo cuja cujos cujas se seu sua seus suas deles delas
-é são está estão era eram na nas no nos os as não ou sim certamente ao aos mais maior maioria mas porém contudo todavia cada qualquer que
+my %palavrasReservadas = map { $_ => 1 } qw(a à às ao aos o os e de desde da das do dos em entre dentro com consigo como ele eles ela elas
+contigo conosco para perante por perante através um uma uns umas que quem cujo cuja cujos cujas se seu sua seus suas deles delas pela pelas
+é são está estão era eram na nas no nos os as não ou sim certamente ao aos mais maior maioria mas porém contudo todavia cada qualquer que faz
 isso isto aquele aquela aqueles aquelas há havia sob sobre durante entre após antes desde contra perante até bem mal pouco poucos com por qual 
-pouca poucas muito muitos muita muitas então depois ainda quando enquanto porque seja sendo fui foi fomos foram me te se nos vos lhe lhes quais);
+pouca poucas muito muitos muita muitas então depois ainda quando enquanto porque seja sendo fui foi fomos foram me te se nos vos lhe lhes quais
+mãe todo toda todos todas);
 
 my %frequencia;  # Hash com a frequencia das palavras
 
@@ -23,6 +24,7 @@ while(my $linha = <$arq>)
     
     foreach my $palavra (@palavras){
         next if exists $palavrasReservadas{$palavra};
+        next if $palavra =~ /^\d+$/;     # Para ignorar numeros
         $frequencia{$palavra}++;
     }
 }
